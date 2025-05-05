3a7f2ce2f946981fe70a2af5974112333dadef11
Kourosh Zahednia <k.zahednia@outlook.com>
2025-05-05 12:01:29 +0330

    save

--- EndPoint/Forms/ComputerDetail/CheckList/FrmCheckListItem.Designer.cs
+++ EndPoint/Forms/ComputerDetail/CheckList/FrmCheckListItem.Designer.cs
@@ -54,9 +54,9 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
-            label1.Location = new Point(143, 81);
+            label1.Location = new Point(125, 61);
             label1.Name = "label1";
-            label1.Size = new Size(61, 32);
+            label1.Size = new Size(50, 25);
             label1.TabIndex = 0;
             label1.Text = "FAN";
             // 
@@ -64,9 +64,9 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
-            label2.Location = new Point(416, 81);
+            label2.Location = new Point(355, 61);
             label2.Name = "label2";
-            label2.Size = new Size(126, 32);
+            label2.Size = new Size(100, 25);
             label2.TabIndex = 1;
             label2.Text = "Hard Disk";
             // 
@@ -74,9 +74,9 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
-            label3.Location = new Point(737, 84);
+            label3.Location = new Point(645, 63);
             label3.Name = "label3";
-            label3.Size = new Size(108, 32);
+            label3.Size = new Size(86, 25);
             label3.TabIndex = 2;
             label3.Text = "Monitor";
             // 
@@ -84,9 +84,9 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
-            label4.Location = new Point(143, 197);
+            label4.Location = new Point(123, 148);
             label4.Name = "label4";
-            label4.Size = new Size(66, 32);
+            label4.Size = new Size(52, 25);
             label4.TabIndex = 3;
             label4.Text = "Case";
             // 
@@ -94,9 +94,9 @@
             // 
             label5.AutoSize = true;
             label5.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
-            label5.Location = new Point(406, 197);
+            label5.Location = new Point(348, 148);
             label5.Name = "label5";
-            label5.Size = new Size(128, 32);
+            label5.Size = new Size(100, 25);
             label5.TabIndex = 4;
             label5.Text = "Anti Virus";
             // 
@@ -104,9 +104,9 @@
             // 
             label6.AutoSize = true;
             label6.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
-            label6.Location = new Point(737, 197);
+            label6.Location = new Point(645, 148);
             label6.Name = "label6";
-            label6.Size = new Size(45, 32);
+            label6.Size = new Size(37, 25);
             label6.TabIndex = 5;
             label6.Text = "OS";
             // 
@@ -114,127 +114,117 @@
             // 
             label7.AutoSize = true;
             label7.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
-            label7.Location = new Point(143, 316);
+            label7.Location = new Point(125, 232);
             label7.Name = "label7";
-            label7.Size = new Size(111, 32);
+            label7.Size = new Size(38, 25);
             label7.TabIndex = 6;
-            label7.Text = "CD ROM";
+            label7.Text = "CD";
             // 
             // label8
             // 
             label8.AutoSize = true;
             label8.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
-            label8.Location = new Point(143, 408);
+            label8.Location = new Point(102, 313);
             label8.Name = "label8";
-            label8.Size = new Size(81, 32);
+            label8.Size = new Size(63, 25);
             label8.TabIndex = 7;
             label8.Text = "Detail";
             // 
             // label9
             // 
             label9.AutoSize = true;
-            label9.Location = new Point(128, 776);
+            label9.Location = new Point(112, 582);
             label9.Name = "label9";
-            label9.Size = new Size(0, 20);
+            label9.Size = new Size(0, 15);
             label9.TabIndex = 8;
             // 
             // CBFan
             // 
             CBFan.FormattingEnabled = true;
-            CBFan.Location = new Point(207, 84);
-            CBFan.Margin = new Padding(3, 4, 3, 4);
+            CBFan.Location = new Point(189, 63);
             CBFan.Name = "CBFan";
-            CBFan.Size = new Size(138, 28);
+            CBFan.Size = new Size(121, 23);
             CBFan.TabIndex = 9;
             // 
             // CBHardDisk
             // 
             CBHardDisk.FormattingEnabled = true;
-            CBHardDisk.Location = new Point(527, 84);
-            CBHardDisk.Margin = new Padding(3, 4, 3, 4);
+            CBHardDisk.Location = new Point(461, 63);
             CBHardDisk.Name = "CBHardDisk";
-            CBHardDisk.Size = new Size(138, 28);
+            CBHardDisk.Size = new Size(121, 23);
             CBHardDisk.TabIndex = 9;
             // 
             // CBMonitor
             // 
             CBMonitor.FormattingEnabled = true;
-            CBMonitor.Location = new Point(842, 84);
-            CBMonitor.Margin = new Padding(3, 4, 3, 4);
+            CBMonitor.Location = new Point(737, 63);
             CBMonitor.Name = "CBMonitor";
-            CBMonitor.Size = new Size(138, 28);
+            CBMonitor.Size = new Size(121, 23);
             CBMonitor.TabIndex = 9;
             // 
             // CBCase
             // 
             CBCase.FormattingEnabled = true;
-            CBCase.Location = new Point(207, 197);
-            CBCase.Margin = new Padding(3, 4, 3, 4);
+            CBCase.Location = new Point(189, 148);
             CBCase.Name = "CBCase";
-            CBCase.Size = new Size(138, 28);
+            CBCase.Size = new Size(121, 23);
             CBCase.TabIndex = 9;
             // 
             // CBAnti
             // 
             CBAnti.FormattingEnabled = true;
-            CBAnti.Location = new Point(527, 197);
-            CBAnti.Margin = new Padding(3, 4, 3, 4);
+            CBAnti.Location = new Point(461, 148);
             CBAnti.Name = "CBAnti";
-            CBAnti.Size = new Size(138, 28);
+            CBAnti.Size = new Size(121, 23);
             CBAnti.TabIndex = 9;
             // 
             // CBOs
             // 
             CBOs.FormattingEnabled = true;
-            CBOs.Location = new Point(842, 200);
-            CBOs.Margin = new Padding(3, 4, 3, 4);
+            CBOs.Location = new Point(737, 150);
             CBOs.Name = "CBOs";
-            CBOs.Size = new Size(138, 28);
+            CBOs.Size = new Size(121, 23);
             CBOs.TabIndex = 9;
             // 
             // CBCD
             // 
             CBCD.FormattingEnabled = true;
-            CBCD.Location = new Point(249, 316);
-            CBCD.Margin = new Padding(3, 4, 3, 4);
+            CBCD.Location = new Point(189, 232);
             CBCD.Name = "CBCD";
-            CBCD.Size = new Size(138, 28);
+            CBCD.Size = new Size(121, 23);
             CBCD.TabIndex = 9;
             // 
             // CBSurveyor
             // 
             CBSurveyor.FormattingEnabled = true;
-            CBSurveyor.Location = new Point(529, 316);
-            CBSurveyor.Margin = new Padding(3, 4, 3, 4);
+            CBSurveyor.Location = new Point(461, 230);
             CBSurveyor.Name = "CBSurveyor";
-            CBSurveyor.Size = new Size(138, 28);
+            CBSurveyor.Size = new Size(121, 23);
             CBSurveyor.TabIndex = 9;
             // 
             // label10
             // 
             label10.AutoSize = true;
             label10.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
-            label10.Location = new Point(416, 316);
+            label10.Location = new Point(348, 230);
             label10.Name = "label10";
-            label10.Size = new Size(117, 32);
+            label10.Size = new Size(93, 25);
             label10.TabIndex = 7;
             label10.Text = "Surveyor";
             // 
             // txtDetail
             // 
-            txtDetail.Location = new Point(216, 421);
-            txtDetail.Margin = new Padding(3, 4, 3, 4);
+            txtDetail.Location = new Point(171, 313);
             txtDetail.Multiline = true;
             txtDetail.Name = "txtDetail";
-            txtDetail.Size = new Size(449, 123);
+            txtDetail.Size = new Size(454, 93);
             txtDetail.TabIndex = 10;
             // 
             // BtnSave
             // 
-            BtnSave.Location = new Point(777, 408);
-            BtnSave.Margin = new Padding(3, 4, 3, 4);
+            BtnSave.Location = new Point(645, 313);
             BtnSave.Name = "BtnSave";
-            BtnSave.Size = new Size(154, 55);
+            BtnSave.Size = new Size(195, 62);
             BtnSave.TabIndex = 11;
             BtnSave.Text = "SAVE";
             BtnSave.UseVisualStyleBackColor = true;
@@ -242,9 +232,9 @@
             // 
             // FrmCheckListItem
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1095, 573);
+            ClientSize = new Size(935, 445);
             Controls.Add(BtnSave);
             Controls.Add(txtDetail);
             Controls.Add(CBSurveyor);
@@ -265,7 +255,6 @@
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
-            Margin = new Padding(3, 4, 3, 4);
             Name = "FrmCheckListItem";
             Text = "FrmItem";
             Load += FrmCheckListItem_Load;
